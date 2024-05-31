605d8733e61f2c48a62d884d63b31fc85573b187
Santhanapriyap <107452002+Santhanapriyap@users.noreply.github.com>
2024-05-31 15:42:49 +0530

    Add files via upload
    Day 1 Assignment - Primitive Data type and Var-let-Const

---
+++ datatype.js
@@ -0,0 +1,15 @@
+var firstName = "Santhanapriya"
+var companyName = "ABCD"
+var mobNumber =9899999999
+var isAutomation = true
+var hasPlaywright;
+
+console.log("First name is:" + firstName);
+console.log("Type of First name is:" + typeof firstName);
+console.log("Company Name is:" + companyName);
+console.log("Type of company Name is:" + typeof companyName);
+console.log("mobNumber Name is:" + mobNumber);
+console.log("Type of mobNumber is:" + typeof mobNumber);
+console.log("Is it Automation:" + isAutomation);
+console.log("Type of isAutomation" + typeof isAutomation);
+console.log("Has Playwright?"+ hasPlaywright);
---
+++ variable.js
@@ -0,0 +1,15 @@
+let gender ='Female'
+function details()
+{
+    var age=32;
+    if(gender.startsWith("Female"))
+    {
+        console.log("She");
+        let age=20;
+    }
+    else
+    {
+        console.log("Boy");
+    }
+    console.let(age);
+}
